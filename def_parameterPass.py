c41874226574c94a87727eb80ac80613c14dea36
udaymondal <58623748+udaymondal@users.noreply.github.com>
2020-07-13 20:08:05 +0600

    Add files via upload

---
+++ def_parameterPass.py
@@ -0,0 +1,11 @@
+def area_triangle(base, height):
+    return ((base*height)/2)
+
+area_a = area_triangle(5,4)
+area_b = area_triangle(7,3)
+
+sum = area_a + area_b
+diff = area_b - area_a
+
+print("The sum of both areas is " + str(sum) + " and difference is " + str(diff))
+

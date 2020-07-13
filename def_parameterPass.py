57a828c838e8936e7670bb2cfded4429f29bdb05
udaymondal <58623748+udaymondal@users.noreply.github.com>
2020-07-13 22:03:49 +0600

    Update def_parameterPass.py

--- def_parameterPass.py
+++ def_parameterPass.py
@@ -9,3 +9,18 @@ diff = area_b - area_a
 
 print("The sum of both areas is " + str(sum) + " and difference is " + str(diff))
 
+
+# parameterpasser 2nd
+
+def month_days (month, days):
+    print(month + " has " + str(days) + " days")
+
+month_days("june","30")
+month_days("july", "31")
+
+month_days("February", "28")
+#namwe = "udaysankarmondal"
+#print(len(namwe))
+
+
+
